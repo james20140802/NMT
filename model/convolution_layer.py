55735c2b6af683389d028ad9f662603980d5e238
SangChu04 <drchasekim@gmail.com>
2019-12-21 16:31:04 +0900

    Chagne "Resnet" to "ResNet" Add comment about stride and is_first_layer Made class named ResNetBlock It is larger component of entire model using ResNetIdentityBlock

--- model/convolution_layer.py
+++ model/convolution_layer.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 import tensorflow as tf
 
 
-class ResnetIdentityBlock(tf.keras.Model):
+class ResNetIdentityBlock(tf.keras.Model):
     """Calculate convolution over sentence"""
     def __init__(self, hidden_size, kernel_size, stride=1, is_first_layer=False):
         """Specify characteristic parameters of convolution block and build the block
@@ -15,12 +15,14 @@ class ResnetIdentityBlock(tf.keras.Model):
         Args:
             hidden_size: Integer, the dimensionality of the output space
             kernel_size: An integer or tuple/list of a single integer, specifying the length of the convolution window
+            stride: An integer or tuple/list of a single integer, specifying the stride length of the convolution
+            is_first_layer: Bool, if it is true, the block uses 1X1 convolution in identity mapping
 
         Raise:
             If hidden size is not dividable by 4
         """
 
-        super(ResnetIdentityBlock, self).__init__()
+        super(ResNetIdentityBlock, self).__init__()
 
         if hidden_size % 4 != 0:
             raise Exception("Hidden size should be dividable by 4")
@@ -81,3 +83,47 @@ class ResnetIdentityBlock(tf.keras.Model):
         outputs = self.relu(outputs)
 
         return outputs
+
+
+class ResNetBlock(tf.keras.Model):
+    """Construct ResNet block using ResNetIdentityBlock"""
+    def __init__(self, hidden_size, kernel_size, stride=1):
+        """Specify characteristic parameters of ResNet block and build the block
+
+        Args:
+            hidden_size: Integer, the dimensionality of the output space
+            kernel_size: An integer or tuple/list of a single integer, specifying the length of the convolution window
+            stride: An integer or tuple/list of a single integer, specifying the stride length of the convolution
+
+        """
+        super(ResNetBlock, self).__init__()
+
+        self.hidden_size = hidden_size
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+        self.block_1 = ResNetIdentityBlock(hidden_size, kernel_size, stride=stride, is_first_layer=True)
+        self.block_2 = ResNetIdentityBlock(hidden_size, kernel_size)
+        self.block_3 = ResNetIdentityBlock(hidden_size, kernel_size)
+
+    def get_config(self):
+        return {
+            "hidden_size": self.hidden_size,
+            "kernel_size": self.kernel_size,
+            "stride": self.stride
+        }
+
+    def call(self, inputs, **kwargs):
+        """Get latent vector by convolution network with residual connection
+
+        Args:
+            inputs: A float32 tensor with [batch_size, length, hidden_size]
+        Returns:
+            outputs: latent vector, float vector with shape [batch_size, length, hidden_size]
+        """
+
+        x = self.block_1(inputs)
+        x = self.block_2(x)
+        x = self.block_3(x)
+
+        return x
